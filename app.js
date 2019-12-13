6091832a4f52663cada824bb207cf2ae3ecbc991
andy <andy.afene-mebaley@epitech.eu>
2019-12-13 16:48:58 +0100

    response convention

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require ('babel-register')
+const func = require('functions')
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
@@ -21,7 +22,17 @@ const members = [
 app.use(morgan('dev'))
 
 app.get('/api/v1/members/:id', (req, res) => {
-    res.send(members[(req.params.id) - 1])
+    res.json(func.success(members[(req.params.id) - 1].name))
+})
+
+app.get('/api/v1/members', (req, res) => {
+    if(req.query.max != undefined && req.query.max > 0){
+        res.json(func.success(members.slice(0, req.query.max)))
+    }else if(req.query.max != undefined){
+        res.json(func.error('Wrong max value'))
+    }else{
+        res.json(func.success(members))
+    }
 })
 
 app.listen(8080, () => console.log('Started on port 8080.'))
