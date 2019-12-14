7c3e91a789fea38de5881525840837085802d24a
andy <andy.afene-mebaley@epitech.eu>
2019-12-14 13:21:35 +0100

    POST parametres

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require ('babel-register')
 const func = require('functions')
+const bodyParser = require('body-parser')
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+
+//--------------------------------------------Members-----------------------------------------------------
 const members = [
     {
         id: 1,
@@ -19,8 +22,16 @@ const members = [
     }
 ]
 
+//---------------------------------middleware-------------------------------------------------------
 app.use(morgan('dev'))
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+
+
+
+
+//-----------------------------------GET--------------------------------------------------------------
 app.get('/api/v1/members/:id', (req, res) => {
     res.json(func.success(members[(req.params.id) - 1].name))
 })
@@ -35,4 +46,42 @@ app.get('/api/v1/members', (req, res) => {
     }
 })
 
+
+
+//--------------------------------------POST-----------------------------------------------------------
+app.post('/api/v1/members', (req, res) => {
+    if(req.body.name) {
+
+        let sameName = false
+
+        for(let i=0; i < members.length; i++) {
+            if(members[i].name == req.body.name){
+                sameName = true
+                break
+            }
+        }
+
+        if(sameName){
+            res.json(func.error('Name already taken'))
+        }else{
+
+            let member = {
+                id: members.length+1,
+                name: req.body.name
+            }
+    
+            members.push(member)
+    
+            res.json(func.success(member))
+        }        
+
+    }else{
+        res.json(func.error('no name value'))
+    }
+  })
+
+
+
+
+//--------------------------------------Listen-----------------------------------------------------------
 app.listen(8080, () => console.log('Started on port 8080.'))
